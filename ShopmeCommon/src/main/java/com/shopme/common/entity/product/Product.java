21b14d13e2b410cd69c148d98bb2f9fb91d439f2
rogerkangren1623 <rogerkangren@yahoo.com>
2022-01-08 11:04:03 -0600

    Fixed the setting not display issue

--- ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/product/Product.java
@@ -27,10 +27,10 @@ import com.shopme.common.entity.IdBaseEntity;
 @Entity
 @Table(name = "products")
 public class Product  extends IdBaseEntity {
-	@Column(unique = true, length = 256, nullable = false)
+	@Column(unique = true, length = 255, nullable = false)
 	private String name;
 
-	@Column(unique = true, length = 256, nullable = false)
+	@Column(unique = true, length = 255, nullable = false)
 	private String alias;
 
 	@Column(length = 512, nullable = false, name = "short_description")
