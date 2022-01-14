091d002d00c9e153746f2f46568f7225e038b5f6
rogerkangren1623 <rogerkangren@yahoo.com>
2022-01-14 09:34:57 -0600

    End of TestGuru-51-Skip-Junit-tests using @Disabled with value

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/MainControllerTest.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/MainControllerTest.java
@@ -24,7 +24,7 @@ public class MainControllerTest {
 		assertEquals("login", controller.viewLoginPage(), "Return view is wrong.");	
 	}
 	
-	@Disabled
+	@Disabled(value="Disabled until we learn how to do it- Note:2022-1-14")
 	@Test
 	public void testViewLoginPageWithAuthentication() {
 		//Disable this test using @Disable since I can not put a authenticated user 
